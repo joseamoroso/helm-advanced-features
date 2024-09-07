0273468c04dd52dbf1003b4e894a7871def25d5b
joseamoroso <joselo.a2@hotmail.com>
2024-09-06 21:38:04 -0500

    feat: added support all pod resources usage

--- plugins/plan-pods-usage/README.md
+++ plugins/plan-pods-usage/README.md
@@ -1,3 +1,48 @@
-TODO:
+# Plan Pods Usage Helm Plugin
 
-- Add usage for other resources like statefullset and daemons.
+## Overview
+
+The `plan-pods-usage` Helm plugin helps you analyze and plan the resource usage of your Kubernetes pods. It extracts and processes resource definitions from your Helm charts, providing insights into CPU and memory allocations.
+
+## Requirements
+
+- `yq`
+
+## Installation
+
+To install the `plan-pods-usage` plugin, follow these steps in the [plugins/plan-pods-usage](./) directory:
+
+```sh
+helm plugin install ./
+```
+
+## Usage
+
+This section provides detailed usage instructions, including the command syntax, description, arguments, options, and example usage scenarios.
+
+```sh
+helm plan-pods-usage [path-to-helm-chart]
+```
+
+### Arguments
+
+- `<chart-path>`: Path to the Helm chart directory.
+
+### Options
+
+- `-h, --help`: Show this help message and exit.
+
+### Example Usage
+
+```sh
+helm plan-pods-usage /path/to/chart
+helm plan-pods-usage /path/to/chart -f /path/to/custom-values.yaml
+helm plan-pods-usage /path/to/chart -f /path/to/custom-values.yaml -n my-namespace
+```
+
+## Clean-up
+
+```sh
+helm plugin uninstall plan-pods-usage
+```
+
--- plugins/plan-pods-usage/scripts/plan-pods-usage.sh
+++ plugins/plan-pods-usage/scripts/plan-pods-usage.sh
@@ -1,39 +1,79 @@
 #!/bin/bash
 
-# Check if helm is installed
-if ! command -v helm &> /dev/null
-then
-    echo "Helm could not be found. Please install Helm to use this plugin."
-    exit 1
+# Function to display usage and describe options
+usage()  {
+  echo "Usage: helm plan-pods-usage [path-to-helm-chart]"
+  echo ""
+  echo "Description:"
+  echo "This script renders a Helm chart to YAML and extracts CPU and RAM resource requests."
+  echo "Arguments:"
+  echo "  <chart-path>        Path to the Helm chart directory."
+  echo "Options:"
+  echo "  -h, --help          Show this help message and exit."
+  echo ""
+  echo "Example Usage:"
+  echo "  helm plan-pods-usage /path/to/chart"
+  echo "  helm plan-pods-usage /path/to/chart -f /path/to/custom-values.yaml"
+  echo "  helm plan-pods-usage /path/to/chart -f /path/to/custom-values.yaml -n my-namespace"
+  echo ""
+}
+
+# Check if the help flag is provided
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+  usage
+  exit 0
 fi
 
+# Check if the chart path is provided
+if [ -z "$1" ]; then
+  echo "Error: No chart path provided."
+  usage
+  exit 1
+fi
+
+# Path to the Helm chart
+chart_path=$1
 # Render the Helm chart to YAML
-rendered_yaml=$(helm template "$@")
+rendered_yaml=$($HELM_BIN template "$chart_path")
 
-# Extract CPU resource requests and limits, considering the number of replicas
-echo "CPU Resources in the Helm chart:"
-echo "$rendered_yaml" | awk '
-/kind: Deployment/ { in_deployment = 1 }
-/replicas:/ { if (in_deployment) replicas = $2 }
-/resources:/ { if (in_deployment) in_resources = 1 }
-/limits:/ { if (in_resources) in_limits = 1 }
-/requests:/ { if (in_resources) in_requests = 1 }
-/cpu:/ {
-    if (in_limits) {
-        limits_cpu = $2
-        total_limits_cpu += limits_cpu * replicas
-        in_limits = 0
-    }
-    if (in_requests) {
-        requests_cpu = $2
-        total_requests_cpu += requests_cpu * replicas
-        in_requests = 0
-    }
-}
-/---/ { in_deployment = 0; in_resources = 0; replicas = 1 }
-END {
-    print "Total CPU Requests: " total_requests_cpu
-    print "Total CPU Limits: " total_limits_cpu
-}'
+deployments=$(echo "$rendered_yaml" | yq e 'select(.kind == "Deployment")' -)
+daemonsets=$(echo "$rendered_yaml" | yq e 'select(.kind == "DaemonSet")' -)
+statefulsets=$(echo "$rendered_yaml" | yq e 'select(.kind == "StatefulSet")' -)
 
+# Initialize an empty array
+resources=()
+
+# Add non-empty results to the array
+if [[ -n "$deployments" ]]; then
+  resources+=("$deployments")
+fi
+
+if [[ -n "$daemonsets" ]]; then
+  resources+=("$daemonsets")
+fi
+
+if [[ -n "$statefulsets" ]]; then
+  resources+=("$statefulsets")
+fi
+
+total_cpu_usage=0
+total_memory_usage=0
+# Print the array elements
+for resource in "${resources[@]}"; do
+  cpu_usage=$(echo "$resource" | yq '.spec.template.spec.containers[].resources.requests.cpu')
+  memory_usage=$(echo "$resource" | yq '.spec.template.spec.containers[].resources.requests.memory')
+  replicas=$(echo "$resource" | yq '.spec.replicas')
+  #validate if replicas is equal to null
+  if [[ $replicas == "null" ]]; then
+    replicas=1
+  fi
+  cpu_request_m=$(echo $cpu_usage | sed 's/m//')
+  memory_request_m=$(echo $memory_usage | sed 's/Mi//')
+  total_cpu_usage=$((total_cpu_usage + cpu_request_m * replicas))
+  total_memory_usage=$((total_memory_usage + memory_request_m * replicas))
+done
+
+echo "Total CPU Requests: $total_cpu_usage"m
+echo "Total RAM Requests: $total_memory_usage"Mi
 exit 0
+
